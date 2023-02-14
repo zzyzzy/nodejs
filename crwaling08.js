009e5c4001e9f563d0f5dd0ef6f5b3b7e71b19b0
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-14 10:44:22 +0900

    k-apt - 관리시설정보, 주차장대수

--- crwaling08.js
+++ crwaling08.js
@@ -17,14 +17,14 @@ async function main() {
         await chrome.wait(until.elementLocated(By.xpath('//a[@title="우리단지 기본정보"]')), 5000);
 
         // 단지정보 버튼 클릭
-        //let menu = await chrome.findElement(By.css('#nav > ul > li:nth-child(1) > a'));
+        // let menu = await chrome.findElement(By.css('#nav > ul > li:nth-child(1) > a'));
         let menu = await chrome.findElement(By.xpath('//a[@title="단지정보"]'));
         await chrome.actions().move({origin: menu})
             .click().perform();
         await sleep(1000);
 
         // 우리단지 기본정보 버튼 클릭
-        //menu = await chrome.findElement(By.css('ul.wp220 li a'));
+        // menu = await chrome.findElement(By.css('ul.wp220 li a'));
         menu = await chrome.findElement(By.xpath('//a[@title="우리단지 기본정보"]'));
         await chrome.actions().move({origin: menu})
             .click().perform();
@@ -67,17 +67,59 @@ async function main() {
         // 검색결과 출력 - 아파트명, 주소
         let apts = await chrome.findElements(By.css('.aptS_rLName'));
         let aptaddrs = await chrome.findElements(By.css('.aptS_rLAdd'));
-        await sleep(3000);
+        await sleep(1500);
 
-        for(let apt of apts) {
+        for(let apt of apts) {  // 아파트이름
             console.log(await apt.getAttribute('textContent'));
         }
-        await sleep(3000);
+        await sleep(1500);
 
-        for(let addr of aptaddrs) {
+        for(let addr of aptaddrs) { // 아파트 주소
             console.log(await addr.getAttribute('textContent'));
         }
-        await chrome.sleep(3000);
+        await chrome.sleep(1500);
+
+        // 아이파크 삼성동 항목을 찾아 인덱스값 추출
+        let idx = 0;
+        for (let val of apts) {
+            console.log(`${idx++} ${await val.getAttribute('textContent')}`);
+            if (await val.getAttribute('textContent') == apt) break;
+        }
+
+        // 추출한 인덱스값을 이용해서 해당 항목 직접 클릭
+        menu = await chrome.findElement(
+            By.css(`.mCSB_container ul li:nth-child(${idx}) a`));
+
+        await chrome.actions().move({origin: menu})
+            .click().perform();
+
+        // await chrome.executeScript(
+        //            'arguments[0].click();', apts[--idx]);
+        await chrome.sleep(1500);
+
+        // ----------------------
+        // 관리시설 정보 클릭
+        await chrome.wait(until.elementLocated(
+            By.css('.lnbNav li:nth-child(3) a')), 5000);
+
+        menu = await chrome.findElement(
+            By.css('.lnbNav li:nth-child(3) a'));
+
+        await chrome.actions().move({origin: menu})
+                .click().perform();
+        await sleep(1000);
+
+        // 지상/지하 주차장 대수 추출
+        let pcnt = await chrome.findElement(
+                   By.css('#kaptd_pcnt')).getText();
+        let pcntu = await chrome.findElement(
+                    By.css('#kaptd_pcntu')).getText();
+        let tpcnt = await chrome.findElement(
+                    By.css('#kaptd_total_pcnt')).getText();
+
+        console.log(pcnt, pcntu, tpcnt);
+        await sleep(3000);
+
 
     } catch (ex) {
         console.log(ex);
